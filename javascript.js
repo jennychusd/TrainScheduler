4285cdfc47f6032fddc14a56e48fc3c38a2f096c
Jenny Chu <jennychu.sd@gmail.com>
2017-03-14 18:11:59 -0700

    schedule from firebase loads on page load

--- javascript.js
+++ javascript.js
@@ -9,14 +9,53 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+database.ref().on("child_added", function(childSnapshot) {
+	var trainName = childSnapshot.val().name;
+	var trainDest = childSnapshot.val().destination;
+	var trainFirst = childSnapshot.val().firstTrain;
+	var trainFreq = childSnapshot.val().frequency;
+
+
+	var currentTime = moment();
+	console.log("Current time: " + currentTime + " & converted: " + moment(currentTime).format("hh:mm"));
+
+	var convertedFreq = trainFreq * 60000;
+
+	var firstTrainTime = moment().hour(trainFirst.substring(0,2)).minute(trainFirst.substring(3,5));
+	console.log("firstTrainTime " + firstTrainTime + " & converted: " + moment(firstTrainTime).format("hh:mm"));
+
+	var minutesAway = convertedFreq - ((currentTime - firstTrainTime) % convertedFreq);
+	console.log("minutesAway: " + minutesAway);
+
+	var convertedMinAway = Math.floor(minutesAway / 60000);
+	console.log("convertedMinAway: " + convertedMinAway);
+
+	var nextArr = currentTime + minutesAway;
+	nextArr = moment(nextArr).format("hh:mm");
+	console.log("nextArr: " + nextArr);
+
+
+	var newRow = $("<tr>")
+		.append($("<td width='14%'>").html(trainName))
+		.append($("<td width='14%'>").html(trainDest))
+		.append($("<td width='14%'>").html(trainFreq + " min"))
+		.append($("<td width='14%'>").html(nextArr))
+		.append($("<td width='14%'>").html(convertedMinAway));
+
+
+	$("#scheduleTable").append(newRow);
+
+})
+
 $("#submitTrain").on("click", function() {
 	event.preventDefault();
 
 	var trainName = $("#trainName").val().trim();
 	var trainDest = $("#trainDest").val().trim();
-	var trainFirst = $("#trainFirst").val().trim();
+	var trainFirst = moment($("#trainFirst").val().trim(), "hh:mm").format("hh:mm");
 	var trainFreq = Number($("#trainFreq").val().trim());
 
+	console.log(moment(trainFirst, "hh:mm"));
 	// input validation
 	if (trainName.length !== 0) {
 		var trainNameValid = true;
@@ -24,15 +63,22 @@ $("#submitTrain").on("click", function() {
 	if (trainDest.length !== 0 && typeof(trainDest) === 'string') {
 		var trainDestValid = true;
 	}
+	if (moment(trainFirst, "hh:mm").isValid()){
+		var startTimeValid = true;
+		console.log("test: " + moment(trainFirst, "hh:mm").format("hh:mm"));
+	} else {
+		console.log("else: " + moment(trainFirst, "hh:mm"))
+	}
 	if (typeof(trainFreq) === 'number') {
 		var trainFreqValid = true;
 	}
 	console.log("trainNameValid: " + trainNameValid);
 	console.log("trainDestValid: " + trainDestValid);
+	console.log("startTimeValid: " + startTimeValid);
 	console.log("trainFreqValid: " + trainFreqValid);
 
 
-	if (trainNameValid === true && trainDestValid === true && trainFreqValid === true) {
+	if (trainNameValid && trainDestValid && startTimeValid && trainFreqValid) {
 		// new temp object to store employee data
 		var newTrain = {
 			name: trainName,
@@ -43,41 +89,12 @@ $("#submitTrain").on("click", function() {
 
 		// upload employee to database
 		database.ref().push(newTrain);
-
-		// moment data
-		var currentTime = moment();
-		console.log("Current time: " + currentTime + " & converted: " + moment(currentTime).format("hh:mm"));
-
-		var convertedFreq = trainFreq * 60000;
-
-		var firstTrainTime = moment().hour(trainFirst.substring(0,2)).minute(trainFirst.substring(3,5));
-		console.log("firstTrainTime " + firstTrainTime + " & converted: " + moment(firstTrainTime).format("hh:mm"));
-
-		var minutesAway = convertedFreq - ((currentTime - firstTrainTime) % convertedFreq);
-		console.log("minutesAway: " + minutesAway);
-
-		var convertedMinAway = Math.floor(minutesAway / 60000);
-		console.log("convertedMinAway: " + convertedMinAway);
-
-		var nextArr = currentTime + minutesAway;
-		nextArr = moment(nextArr).format("hh:mm");
-		console.log("nextArr: " + nextArr);
-
-		// create new table row to populate new employee data from form
-		var newRow = $("<tr>")
-			.append($("<td width='14%'>").html(trainName))
-			.append($("<td width='14%'>").html(trainDest))
-			.append($("<td width='14%'>").html(trainFreq + " min"))
-			.append($("<td width='14%'>").html(nextArr))
-			.append($("<td width='14%'>").html(convertedMinAway));
-
-
-		$("#scheduleTable").append(newRow);
-
-
-		$("#trainName").val("");
-		$("#trainDest").val("");
-		$("#trainFirst").val("");
-		$("#trainFreq").val("");
 	}
+
+	$("#trainName").val("");
+	$("#trainDest").val("");	
+	$("#trainFirst").val("");
+	$("#trainFreq").val("");
 })
+
+console.log(database);
